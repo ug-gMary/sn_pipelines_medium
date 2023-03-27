9e0c58b7085faad3510d0bd56eac8358a0d935fb
ug-gMary <grace.biju@ultragenicglobal.com>
2023-03-27 16:27:33 +0530

    Astra-dev-environment-start-stop -testing

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
-print (" The sample python file has been invoked")
+#!/usr/bin/python
+
+import sys
+
+action = sys.argv[1] 
+
+def test_function (action):
+    if action == 'stop':
+        print("The Astra Dev Environment is stopped")
+    if action == 'start':
+        print("The Astra Dev Environment is started")
+    if action == 'status':
+        print("The Astra Dev Environment status is displayed")
+
+print("The python script has been invoked")
+test_function (action)
